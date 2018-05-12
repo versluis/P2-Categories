fea94a0ceef072e2149217ddbbc86aec27814c98
Jay Versluis <jay@versluis.com>
2018-05-12 13:02:47 -0400

    added changelog file

---
+++ Changelog.php
@@ -0,0 +1,11 @@
+<?php
+
+/* P2 Categories - CHANGELOG
+
+v1.6 - May 2018
+* added this changelog file (yey!)
+* rebuilt P2 Categories from my notes
+* based on P2 v1.5.8 (2016)
+* hunted down undocumented features
+
+?>
