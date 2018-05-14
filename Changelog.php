8e596f39e299e63cec08c3190abd0f9d540f657a
Jay Versluis <jay@versluis.com>
2018-05-14 17:15:44 -0400

    updated log for previous versions

--- Changelog.php
+++ Changelog.php
@@ -3,10 +3,43 @@
 /* P2 Categories - CHANGELOG
 
 v1.6 - May 2018
+* this version is a complete re-write
 * rebuilt P2 Categories from my notes, based on P2 v1.5.8 (2016)
-* fixed a bug that did not displayed number of posts in more than one category 
-* fixed use of deprecated function in P2 Source
+* fixed the category drop down menu
+* fixed a bug that did not displayed the number of posts in more than one category 
+* updated a call to a deprecated WordPress function wp_get_current_user()
+* fixed several PHP 7 deprecation warnings
+* updated class constructors to use __construct() methods
 * hunted down undocumented features and documented them
 * added this changelog file (yey!)
+
+v1.5 - sometime in 2013
+* unreleased beta version
+
+v1.4 - October 2013
+* removed category display from archive pages
+* categories are not shown if posts are in "uncategorized"
+* added post count to category meta display (just like P2 tags)
+* turned project back into a full fork again, it's no longer a child theme (category posting didn't work as a child, hence the change back)
+* can be used with other P2 child themes, such as P2 Responsive https://github.com/nmec/p2-responsive
+
+v1.3
+* unreleased beta version
+
+v1.2
+* turned project into a child theme
+
+v1.1 
+* added dropdown menu for category selection (thanks to Nobble)
+* display categories in each post
+* added Category Archives page
+* fixed "broken menu in Firefox" when using a custom header (thanks to lagerassasin)
+* minor CSS improvements for PRE tags and search submit button
+* added P2 Turkish translation (thanks to erayaydin)
+* added Brazilian Portuguese translation (thanks to Eduardo Zulian)
+
+v1.0
+* initial release, based on P2 1.5.1
+* added support for posting into categories, as it was in P2 1.3.3
 
 ?>
